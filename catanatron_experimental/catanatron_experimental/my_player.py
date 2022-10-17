e98c245f636cff489aa2afd51dabf5e24d92f787
NewLaptop <piszczekwiktor@gmail.com>
2022-10-17 14:59:59 +0200

    setting up my_player

--- catanatron_experimental/catanatron_experimental/my_player.py
+++ catanatron_experimental/catanatron_experimental/my_player.py
@@ -1,20 +1,27 @@
 from typing import Iterable
-
 from catanatron.game import Game
 from catanatron.models.actions import Action
 from catanatron.models.player import Player
+import numpy as np
 
 
 class MyPlayer(Player):
 
-    def __init__(self, color, genome, net, is_bot=True,):
+    def __init__(self, genome, net, neat_env, color, is_bot=True,):
         super().__init__(color, is_bot)
         self.genome = genome
         self.net = net
+        self.neat_env = neat_env
 
     def decide(self, game: Game, playable_actions: Iterable[Action]):
 
+        vectors = self.neat_env.game_to_vector(playable_actions)
+        ratings = []
+        for vector in vectors:
+            rating = self.net.activate(vector)
+            ratings.append(rating)
+
+        id = np.argmax(ratings)
 
         # for the sake of testing
-        return playable_actions[0]
-
+        return playable_actions[id]
--- sample.py
+++ sample.py
@@ -2,17 +2,29 @@ from catanatron import Game, RandomPlayer, Color
 from catanatron_experimental.my_player import MyPlayer
 from catanatron_server.utils import open_link
 import os
+import neat
+from NEAT.neat_env import NeatEnv
 
 
 def main(genomes, config):
     # Play a simple 4v4 game. Edit MyPlayer with your logic!
-    players = [
-        RandomPlayer(Color.RED),
-        RandomPlayer(Color.BLUE),
-        RandomPlayer(Color.WHITE),
-        RandomPlayer(Color.ORANGE),
-    ]
+    nets = []
+    ge = []
+    for id, g in genomes:
+        # setting up genomes, connecting them and appending to the genome list named ge
+
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+        ge.append(g)
+
+    colors = [Color.RED, Color.BLUE, Color.WHITE, Color.ORANGE]
+    players = [MyPlayer(genome, net, 0, color) for genome, net, color in zip(ge, nets, colors)]
     game = Game(players)
+    neat_env = NeatEnv(game)
+    for player in players:
+        player.neat_env = neat_env
+
     print(game.play())  # returns winning color
 
     # Ensure you have `docker-compose up` running
@@ -20,8 +32,25 @@ def main(genomes, config):
     # open_link(game)  # opens game result in browser
 
 
+def run(config_path):
+    # those match the topic in configuration file, those names down there,
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+
+    # showing stats instead of black running screan
+    p.add_reporter(neat.Checkpointer(60))
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 1)
+    print('\nBest genome:\n{!s}'.format(winner))
+    return winner, p.config
+
+
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'NEAT_config.txt')
-    main(0, 0)
-    #run(config_path)
+    run(config_path)
